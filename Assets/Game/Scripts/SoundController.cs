5d2f65db4e38e3cdbed1bde12b397d931ec18cf7
CemHaspolat <73745125+cemhas@users.noreply.github.com>
2022-10-26 14:34:11 +0300

    Add: soundcontroller

---
+++ Assets/Game/Scripts/SoundController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+namespace Game.Scripts
+{
+    public class SoundController : MonoBehaviour
+    {
+        [SerializeField] private AudioSource _audioSource;
+        [SerializeField] private RightHandBehaviour _rightHandBehaviour;
+
+        [SerializeField] private AudioClip _generalSound;
+        [SerializeField] private AudioClip _clip1;
+        [SerializeField] private AudioClip _clip2;
+        [SerializeField] private AudioClip _clip3;
+        [SerializeField] private AudioClip _clip4;
+
+        private void Start()
+        {
+            _rightHandBehaviour.StartButtonInteracted += OnStartButtonInteracted;
+            _rightHandBehaviour.NameButtonInteracted += OnNameButtonInteracted;
+        }
+
+        private void OnStartButtonInteracted()
+        {
+            _audioSource.clip = _generalSound;
+            _audioSource.Play();
+        }
+
+        private void OnNameButtonInteracted(string name)
+        {
+            _audioSource.Stop();
+
+            if (name == "Name1")
+            {
+                _audioSource.clip = _clip1;
+                _audioSource.Play();
+            }
+            else if (name == "Name2")
+            {
+                _audioSource.clip = _clip2;
+                _audioSource.Play();
+            }
+            else if (name == "Name3")
+            {
+                _audioSource.clip = _clip3;
+                _audioSource.Play();
+            }
+            else if (name == "Name4")
+            {
+                _audioSource.clip = _clip4;
+                _audioSource.Play();
+            }
+                
+        }
+        
+    }
+}
